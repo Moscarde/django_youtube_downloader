4825929c5c832e4673348e27e5f732a7940ee18b
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-20 22:37:57 -0300

    fix: fix  video download functionality with slugified filenames and output path handling

--- youtube_downloader/tasks.py
+++ youtube_downloader/tasks.py
@@ -2,6 +2,7 @@ import os
 import re
 
 from celery import shared_task
+from slugify import slugify
 from yt_dlp import YoutubeDL
 
 from .models import Video
@@ -61,16 +62,21 @@ def get_video_info(video_url):
 
 def download_video(video_url, output_path):
     options = {
-        "outtmpl": f"%(title)s.%(ext)s",  # Nome do arquivo de saída
+        "outtmpl": f"{output_path}/%(title)s.%(ext)s",  # Nome do arquivo de saída
         "format": "bestvideo+bestaudio/best",  # Melhor qualidade disponível
     }
     try:
         with YoutubeDL(options) as ydl:
             info_dict = ydl.extract_info(video_url, download=True)
             filename = ydl.prepare_filename(info_dict)
-            filename = output_path + "/" + re.sub(r"[^\w\-. ]", "_", filename)
+            base_name, ext = os.path.splitext(os.path.basename(filename))
+            slugified_name = slugify(base_name)
+            slugified_filename = f"{slugified_name}{ext}"
+            final_path = os.path.join(os.path.dirname(filename), slugified_filename)
+            os.rename(filename, final_path)
+
         print("Download concluído!")
-        return filename
+        return final_path
     except Exception as e:
         print(f"Erro ao baixar o vídeo: {e}")
         return None
